55859151c867027fa83d06cb1ceb6e41d342e77d
Buliyamba <148887446+Buliyamba@users.noreply.github.com>
2026-07-17 22:06:28 +0500

    Update functions for its better look and usability

--- Answers/Module1/lesson6_step10.py
+++ Answers/Module1/lesson6_step10.py
@@ -3,25 +3,33 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-try:
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Firefox()
-    browser.get(link)
 
+def insert_required_data(driver, current_link):
+    driver.get(current_link)
     # Выбор всех обязательных полей и их заполнение
-    elements = browser.find_elements(By.XPATH, "//label[contains(.,'*')]/following-sibling::input")
-    for element in elements:
-        element.send_keys("Абоба")
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    first_name = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your first name'][required]")
+    last_name = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your last name'][required]")
+    email = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Input your email'][required]")
+    fields = [first_name, last_name, email]
+    for field in fields:
+        field.send_keys("абоба")
+
+    button = driver.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
     # Загрузка страницы
     time.sleep(1)
 
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    welcome_text_elt = driver.find_element(By.TAG_NAME, "h1")
     welcome_text = welcome_text_elt.text
 
     assert "Congratulations! You have successfully registered!" == welcome_text
 
-finally:
+
+if __name__ == "__main__":
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Firefox()
+
+    insert_required_data(browser, link)
+
     browser.quit()
